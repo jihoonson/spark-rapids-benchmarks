8ef5885e7556c42de062dfb556321baba254f363
Jihoon Son <ghoonson@gmail.com>
2025-02-20 11:45:17 -0800

    fix writing

--- nds/nds_power.py
+++ nds/nds_power.py
@@ -179,8 +179,7 @@ def run_one_query(spark_session,
             ensure_valid_column_names(df).write.format(output_format).mode('overwrite').save(
                     output_path + '/' + query_name)
         if plan_path:
-            with open(plan_path, 'w') as f:
-                f.write(df.explain(extended=True))
+            spark_session.read.json(df.queryExecution.optimizedPlan.toJSON).write.format("json").mode("overwrite").save(plan_path + '/' + query_name)
 
 def ensure_valid_column_names(df: DataFrame):
     def is_column_start(char):
